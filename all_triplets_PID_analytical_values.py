65655d9fc5a6e08708ec09678bf28aa2c05ca884
SigmaField <5673484+SigmaField@users.noreply.github.com>
2025-08-25 02:46:44 +0200

    fixed bug telated to duplication of triplet (Fz,Cz,Pz)

--- all_triplets_PID_analytical_values.py
+++ all_triplets_PID_analytical_values.py
@@ -12,6 +12,8 @@ left_channels  = ['EEG Fp1', 'EEG F3', 'EEG F7', 'EEG T3', 'EEG C3', 'EEG T5', '
 right_channels = ['EEG Fp2', 'EEG F4', 'EEG F8', 'EEG T4', 'EEG C4', 'EEG T6', 'EEG P4', 'EEG O2', 'EEG Fz', 'EEG Cz', 'EEG Pz']
 left_triplets  = [list(triplet) for triplet in combinations(left_channels,3)]
 right_triplets = [list(triplet) for triplet in combinations(right_channels,3) if triplet!=('EEG Fz', 'EEG Cz', 'EEG Pz')] #triplet (EEG Fz,EEG Cz,EEG Pz) already in left channels, avoid repeat in right channels
+left_pairs     = list(combinations(left_channels,2))
+right_pairs    = list(combinations(right_channels,2))
 
 def get_file_paths_from_config():
   current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -35,8 +37,8 @@ def main():
     Path(rslts_folder+"\\task").mkdir(exist_ok=True)
    
     subject_IDs = args.subjectids
-    stage       = "rest" if args.stageid  == "1" else "task"
-    stageid     = "1" if stage=="rest" else "2"
+    stageid     = args.stageid
+    stage       = "rest" if stageid == "1" else "task"
     start = time.time()
     for ID in subject_IDs:
         print('\nCurrently processing test subject', str(ID),'...',end='\n\n')
@@ -44,26 +46,12 @@ def main():
         result_path_full = rslts_folder + stage + '\\Subject' + str(ID) + "_PID_" + stage + "_128Hz_continuous_analytical.csv"
         raw_data         = load_subjet_data(data_path_full)
         useful_raw_data  = only_useful_data(raw_data,dataset='ari',stage=stage)
-
-        left_MIs         = {frozenset(pair):mi(useful_raw_data[pair[0]],useful_raw_data[pair[1]]) for pair in combinations(left_channels,2)} 
-        right_MIs        = {frozenset(pair):mi(useful_raw_data[pair[0]],useful_raw_data[pair[1]]) for pair in combinations(right_channels,2) if not ('z' in pair[0] and 'z' in pair[1])} 
-
-        results          = {'source1':[],'source2':[],'target' :[],'sinergy':[],'unique1':[],'unique2':[],'redundancy':[]}
+        left_MIs         = {frozenset(pair):mi(useful_raw_data[pair[0]],useful_raw_data[pair[1]]) for pair in left_pairs} 
+        right_MIs        = {frozenset(pair):mi(useful_raw_data[pair[0]],useful_raw_data[pair[1]]) for pair in right_pairs} 
+        results          = {'source1':[],'source2':[],'target' :[],'sinergy':[],'redundancy':[]}
         compute_and_store_analytical_results(useful_raw_data, left_triplets, right_triplets,left_MIs,right_MIs, results, result_path_full)
-
         print('\nFinished!\n\n')
     end = time.time()
     print("ELAPSED TIME:",(end-start)/60)
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
